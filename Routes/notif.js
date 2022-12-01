f3d687052dffb5f7a9730a38f4ff00e947e6cae5
Zenric23 <zenricclarete12344@gmail.com>
2022-12-01 14:26:50 +0800

    add notifications display sort

--- Routes/notif.js
+++ Routes/notif.js
@@ -3,22 +3,25 @@ const Notification = require("../Model/Notification");
 const { getTotalDocuments } = require("../util/getTotalDocuments");
 const { verifyTokenAndAdmin } = require("./verifyToken");
 
+
 router.get("/", async (req, res) => {
   const page = req.query.p || 0;
   let notifPerPage = 10;
+  let reviews
+  let totalNotifs
 
   try {
       
       if (req.query.latest) {
-        notifPerPage = 6
+        notifPerPage = 5
 
-      const reviews = await Notification
+      reviews = await Notification
         .find()
         .skip(notifPerPage * page)
         .limit(notifPerPage)
         .sort({ createdAt: -1 });
 
-      const totalNotifs = await getTotalDocuments(
+      totalNotifs = await getTotalDocuments(
         Notification,
         page,
         notifPerPage,
@@ -33,27 +36,61 @@ router.get("/", async (req, res) => {
 
     }
 
-    const reviews = await Notification.find()
+    const sort = req.query.sort
+
+    if(sort === 'read') {
+      reviews = await Notification.find({isRead: true})
       .skip(notifPerPage * page)
       .limit(notifPerPage)
       .sort({ createdAt: -1 });
 
-    const totalNotifs = await getTotalDocuments(
-      Notification,
-      page,
-      notifPerPage,
-      { createdAt: -1 },
-      null
-    );
+      totalNotifs = await getTotalDocuments(
+        Notification,
+        page,
+        notifPerPage,
+        { createdAt: -1 },
+        { isRead: true }
+      );
+
+    } else if (sort === 'unread') {
+      reviews = await Notification.find({isRead: false})
+      .skip(notifPerPage * page)
+      .limit(notifPerPage)
+      .sort({ createdAt: -1 });
+
+      totalNotifs = await getTotalDocuments(
+        Notification,
+        page,
+        notifPerPage,
+        { createdAt: -1 },
+        { isRead: false }
+      );
+
+    } else {
+      reviews = await Notification.find()
+      .skip(notifPerPage * page)
+      .limit(notifPerPage)
+      .sort({ createdAt: -1 });
+
+      totalNotifs = await getTotalDocuments(
+        Notification,
+        page,
+        notifPerPage,
+        { createdAt: -1 },
+        null
+      );
+    }
 
     const totalUnread = await Notification.find({ isRead: false }).count();
 
     res.status(200).json({ reviews, totalUnread, totalNotifs });
+
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 });
+
 
 router.put("/:id", async (req, res) => {
   try {
